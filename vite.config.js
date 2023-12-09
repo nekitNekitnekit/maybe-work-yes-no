1c78473cc7dc71f9ae4d4cfe2851ab362303f792
nekitNekitnekit <klientkv0711@gmail.com>
2023-12-09 05:53:27 +0200

    woo

--- vite.config.js
+++ vite.config.js
@@ -7,93 +7,79 @@ import zipPack from "vite-plugin-zip-pack";
 
 export default defineConfig({
 
-
-
-///////////////////////////////////////1
-
-	base: '/potential-site/',
-
-//////////////////////////////////////1
+	base: '/maybe-work-yes-no/',
 
 	css: {
 		devSourcemap: true,
 	},
+
 	plugins: [
 		handlebars({
 			partialDirectory: resolve(__dirname, "partials"),
 		}),
-//////////////////////////////////////2
+
 		simpleHtmlPlugin({
 			minify: true,
 		}),
-//////////////////////////////////////
-ViteImageOptimizer({
-		exclude: undefined,
-		include: undefined,
-		includePublic: true,
-		logStats: true,
-		ansiColors: true,
-		svg: {
-		  multipass: true,
-		  plugins: [
-			{
-			  name: 'preset-default',
-			  params: {
-				overrides: {
-				  cleanupNumericValues: false,
-				  removeViewBox: false, // https://github.com/svg/svgo/issues/1128
-				},
-				cleanupIDs: {
-				  minify: false,
-				  remove: false,
-				},
-				convertPathData: false,
-			  },
-			},
-			'sortAttrs',
-			{
-			  name: 'addAttributesToSVGElement',
-			  params: {
-				attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
-			  },
-			},
-		  ],
-		},
-		png: {
-		  // https://sharp.pixelplumbing.com/api-output#png
-		  quality: 100,
-		},
-		jpeg: {
-		  // https://sharp.pixelplumbing.com/api-output#jpeg
-		  quality: 80,
-		},
-		jpg: {
-		  // https://sharp.pixelplumbing.com/api-output#jpeg
-		  quality: 80,
-		},
-		tiff: {
-		  // https://sharp.pixelplumbing.com/api-output#tiff
-		  quality: 100,
-		},
-		// gif does not support lossless compression
-		// https://sharp.pixelplumbing.com/api-output#gif
-		gif: {},
-		webp: {
-		  // https://sharp.pixelplumbing.com/api-output#webp
-		  lossless: true,
-		},
-		avif: {
-		  // https://sharp.pixelplumbing.com/api-output#avif
-		  lossless: true,
-		},
-		cache: false,
-		cacheLocation: undefined,
-  }),
 
-  zipPack()
-/////////////////////////////////////2
+		ViteImageOptimizer({
+				exclude: undefined,
+				include: undefined,
+				includePublic: true,
+				logStats: true,
+				ansiColors: true,
+				svg: {
+				multipass: true,
+				plugins: [
+					{
+					name: 'preset-default',
+					params: {
+						overrides: {
+						cleanupNumericValues: false,
+						removeViewBox: false,
+						},
+						cleanupIDs: {
+						minify: false,
+						remove: false,
+						},
+						convertPathData: false,
+					},
+					},
+					'sortAttrs',
+					{
+					name: 'addAttributesToSVGElement',
+					params: {
+						attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+					},
+					},
+				],
+				},
+				png: {
+				quality: 90,
+				},
+				jpeg: {
+				quality: 90,
+				},
+				jpg: {
+				quality: 90,
+				},
+				tiff: {
+				quality: 90,
+				},
+				gif: {},
+				webp: {
+				lossless: true,
+				},
+				avif: {
+				lossless: true,
+				},
+				cache: false,
+				cacheLocation: undefined,
+		}),
+
+		zipPack()
 	],
-////////////////////////////////////3
+
 	build: {
         rollupOptions: {
             output: {
@@ -105,5 +91,4 @@ ViteImageOptimizer({
             }
         }
     }
-///////////////////////////////////3
 });
